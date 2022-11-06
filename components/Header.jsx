c269cb267778a8388229ed41a29d38140e1d5e57
Gergely Dancs <dancs.gergely@flexoon.hu>
2022-11-06 16:17:31 +0100

    Add page transition

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,29 @@
 import styles from "../styles/Header.module.css";
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
     <div className={styles.header_container}>
-      <div>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            scale: 0.8,
+            opacity: 0,
+          },
+          visible: {
+            scale: 1,
+            opacity: 1,
+            transition: {
+              delay: 0.4,
+            },
+          },
+        }}
+      >
         <h1 className={styles.title}>Gy:M O'CLOCK</h1>
         <h2 className={styles.sub_title}>A MINŐSÉG A VÉDJEGYÜNK!</h2>
-      </div>
+      </motion.div>
       <div className={styles.description_container}>
         <p className={styles.description}>
           EDZŐTERMI FELSZERELÉSEK SZÉLES VÁLASZTÉKA, AKÁR OTTHONI
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import { motion } from "framer-motion";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
     </Layout>
   );
 }
