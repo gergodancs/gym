fda92a52d6b2aea7e06f93b843fb1b3e3abcbe1f
Gergely Dancs <dancs.gergely@flexoon.hu>
2022-11-07 17:42:17 +0100

    Add footer

--- pages/products.jsx
+++ pages/products.jsx
@@ -3,21 +3,30 @@ import { PRODUCTS } from "../helpers/products";
 import styles from "../styles/product/Product.module.css";
 
 const products = () => {
+  const scrollUp = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
-    <div className={styles.products_container}>
-      {PRODUCTS?.map((product, i) => {
-        return (
-          <Product
-            key={i}
-            timing={i}
-            title={product.title}
-            src={product.src}
-            description={product.description}
-            price={product.price}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className={styles.products_container}>
+        {PRODUCTS?.map((product, i) => {
+          return (
+            <Product
+              key={i}
+              timing={i}
+              title={product.title}
+              src={product.src}
+              description={product.description}
+              price={product.price}
+            />
+          );
+        })}
+      </div>
+      <div onClick={scrollUp} className={styles.upto_btn}>
+        Ugrás a lap tetejére
+      </div>
+    </>
   );
 };
 
