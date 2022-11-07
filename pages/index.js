db2bc3e8a0a45db19f34376cb1d42844039d6770
Gergely Dancs <dancs.gergely@flexoon.hu>
2022-11-07 13:47:56 +0100

    fix mobile view

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import Layout from "../components/Layout";
 import Header from "../components/Header";
-import home from "./home";
 import ProductsBtn from "../components/ProductsBtn";
 
 export default function Home() {
