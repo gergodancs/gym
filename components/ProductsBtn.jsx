21f3486929e588e1f9ace05daf6f135724cbc165
Gergely Dancs <dancs.gergely@flexoon.hu>
2022-11-07 16:00:31 +0100

    Add animation

--- components/ProductsBtn.jsx
+++ components/ProductsBtn.jsx
@@ -1,10 +1,24 @@
 import styles from "../styles/ProductsBtn.module.css";
 import Link from "next/link";
+import { motion } from "framer-motion";
+import { BsFillForwardFill } from "react-icons/bs";
 const ProductsBtn = () => {
   return (
-    <div className={styles.products_btn}>
-      <Link href="/products">ERŐKERETEINK >></Link>
-    </div>
+    <motion.div
+      className={styles.products_btn}
+      initial={{
+        opacity: 0,
+        translateX: -250,
+        scale: 0,
+      }}
+      animate={{ opacity: 1, translateX: 0, scale: [0, 1, 1.1, 1] }}
+      transition={{ duration: 0.4, delay: 2.5 }}
+    >
+      <Link href="/products">ERŐKERETEINK</Link>
+      <Link href="/products">
+        <BsFillForwardFill size={40} />
+      </Link>
+    </motion.div>
   );
 };
 
