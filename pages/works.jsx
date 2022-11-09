52177bd3d3d3f6e24f4f5dc031a9291c8d46b601
Gergely Dancs <dancs.gergely@flexoon.hu>
2022-11-09 17:06:39 +0100

    Add grid layout to works

--- pages/works.jsx
+++ pages/works.jsx
@@ -4,11 +4,29 @@ import Image from "next/image";
 import bordas from "../public/carousel/bordasfal.jpg";
 import egyedi from "../public/carousel/egyedi_erokeret.jpg";
 import revolver from "../public/carousel/revolver.jpg";
+import majomletra from "../public/carousel/majomletra.jpg";
+import csigas from "../public/carousel/csigas_erokeret.jpg";
+import hex from "../public/carousel/Hexbar.jpg";
+import nagykeret from "../public/carousel/nagykeret.jpg";
+import pad from "../public/carousel/pad.jpg";
+
+import sled from "../public/carousel/szan.jpg";
 
 import { motion } from "framer-motion";
 
 const works = () => {
-  const pics = [revolver, bordas, egyedi];
+  const pics = [
+    egyedi,
+    csigas,
+    nagykeret,
+    bordas,
+    majomletra,
+    revolver,
+    hex,
+
+    sled,
+    pad,
+  ];
   const [small, setSmall] = useState(true);
 
   return (
