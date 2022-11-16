7f1780c04a50f1aac3036ddc895ccf31f1d525b9
Gergely Dancs <dancs.gergely@flexoon.hu>
2022-11-16 18:22:18 +0100

    Add product to form teszt

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,9 +1,14 @@
+import { useState, useEffect } from "react";
 import styles from "../styles/Form.module.css";
-
+import { motion, AnimatePresence } from "framer-motion";
 import { TiArrowBack } from "react-icons/ti";
 import { RiMailSendFill } from "react-icons/ri";
 
 const Form = (props) => {
+  const [product, setProduct] = useState("");
+  useEffect(() => {
+    setProduct(props.title);
+  }, []);
   const onClickBack = () => props.setShowForm(false);
   return (
     <div className={props.showForm ? styles.show_form : styles.hidden}>
@@ -23,17 +28,24 @@ const Form = (props) => {
         data-netlify="true"
       >
         <input type="hidden" name="form-name" value="contact" />
-        <label htmlFor="name">Név:</label>
-        <input type="text" name="name" placeholder="Név" />
-        <label htmlFor="email">Email:</label>
-        <input type="email" name="email" placeholder="Email" />
-        <label htmlFor="phone">Telefonszám:</label>
-        <input type="text" name="phone" placeholder="Telefonszám" />
+        <input type="hidden" name="product" value={product} />
+        <input type="text" name="name" />
+        <label htmlFor="name">Név</label>
+        <input type="email" name="email" />
+        <label htmlFor="email">Email</label>
+        <input type="text" name="phone" />
+        <label htmlFor="phone">Telefonszám</label>
 
         <button className={styles.formBtn} type="submit">
-          <RiMailSendFill size={95} />
+          Elküld
+          <RiMailSendFill size={30} />
         </button>
       </form>
+      <p className={styles.p}>
+        A rendelés elküldése után, munkatársaink 24 órán belül felveszik Önnel a
+        kapcsolatot, hogy megtudjuk pontos igényeit. Eszközeinket megrendelésre
+        gyártjuk, rövid határidővel
+      </p>
     </div>
   );
 };
